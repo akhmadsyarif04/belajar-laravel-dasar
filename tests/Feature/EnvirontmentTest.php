ba5919869ff1476cf479da193ea198ea9a00c60b
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2023-06-08 19:00:21 +0800

    environtment belum selesai

---
+++ tests/Feature/EnvirontmentTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EnvirontmentTest extends TestCase
+{
+    public function testGetEnv() 
+    {
+        $youtube = env("YOUTUBE");
+
+        self::assertEquals("akhmad syarif", $youtube);
+    }
+}
